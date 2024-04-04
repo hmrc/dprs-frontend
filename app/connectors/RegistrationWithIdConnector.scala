16a7d1c005b717d216c0345fb2d95baf41ad088e
Sean Rasmussen <sean.rasmussen@digital.hmrc.gov.uk>
2024-04-04 15:48:06 +0100

    [ODPR-507] Added support for registration for ID.

---
+++ app/connectors/RegistrationWithIdConnector.scala
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package connectors
+
+import play.api.libs.functional.syntax.{toFunctionalBuilderOps, unlift}
+import play.api.libs.json.{JsPath, OWrites, Reads}
+
+object RegistrationWithIdConnector {
+
+  object Requests {
+
+    final case class Id(idType: String, value: String)
+
+    object Id {
+      implicit lazy val writes: OWrites[Id] =
+        ((JsPath \ "type").write[String] and
+          (JsPath \ "value").write[String])(unlift(Id.unapply))
+    }
+
+  }
+
+  object Responses {
+
+    final case class Id(idType: String, value: String)
+
+    object Id {
+      implicit lazy val reads: Reads[Id] =
+        ((JsPath \ "type").read[String] and
+          (JsPath \ "value").read[String])(Id.apply _)
+    }
+
+    final case class Address(lineOne: String,
+                             lineTwo: Option[String],
+                             lineThree: Option[String],
+                             lineFour: Option[String],
+                             postalCode: String,
+                             countryCode: String
+    )
+
+    object Address {
+      implicit lazy val reads: Reads[Address] =
+        ((JsPath \ "lineOne").read[String] and
+          (JsPath \ "lineTwo").readNullable[String] and
+          (JsPath \ "lineThree").readNullable[String] and
+          (JsPath \ "lineFour").readNullable[String] and
+          (JsPath \ "postalCode").read[String] and
+          (JsPath \ "countryCode").read[String])(Address.apply _)
+    }
+
+    final case class ContactDetails(landline: Option[String], mobile: Option[String], fax: Option[String], emailAddress: Option[String])
+
+    object ContactDetails {
+      implicit lazy val reads: Reads[ContactDetails] =
+        ((JsPath \ "landline").readNullable[String] and
+          (JsPath \ "mobile").readNullable[String] and
+          (JsPath \ "fax").readNullable[String] and
+          (JsPath \ "emailAddress").readNullable[String])(ContactDetails.apply _)
+    }
+
+  }
+
+}
---
+++ it/test/services/BaseConnectorIntSpec.scala
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package services
+
+import com.github.tomakehurst.wiremock.client.WireMock.getAllServeEvents
+import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.time.SpanSugar.convertIntToGrainOfTime
+import org.scalatestplus.play.guice.GuiceOneServerPerSuite
+import play.api.Application
+import play.api.inject.guice.GuiceApplicationBuilder
+import uk.gov.hmrc.http.test.WireMockSupport
+
+import scala.concurrent.{Await, Awaitable, ExecutionContext}
+import scala.jdk.CollectionConverters.CollectionHasAsScala
+
+abstract class BaseConnectorIntSpec
+    extends AnyFreeSpec
+    with Matchers
+    with ScalaFutures
+    with IntegrationPatience
+    with GuiceOneServerPerSuite
+    with WireMockSupport {
+
+  implicit val executionContext: ExecutionContext = app.injector.instanceOf[ExecutionContext]
+
+  def connectorName: String
+
+  def extraApplicationConfig: Map[String, Any] = Map(
+    s"microservice.services.$connectorName.host" -> wireMockHost,
+    s"microservice.services.$connectorName.port" -> wireMockPort
+  )
+
+  def await[T](awaitable: Awaitable[T]): T = Await.result(awaitable, 1.second)
+
+  override def fakeApplication(): Application =
+    baseApplicationBuilder()
+      .configure(extraApplicationConfig)
+      .build()
+
+  def verifyThatDownstreamApiWasCalled(): Unit =
+    withClue("We expected a single downstream API (stub) to be called, but it wasn't.") {
+      getAllServeEvents.asScala.count(_.getWasMatched) shouldBe 1
+    }
+
+  private def baseApplicationBuilder(): GuiceApplicationBuilder =
+    GuiceApplicationBuilder()
+      .configure(
+        "metrics.enabled" -> false
+      )
+
+}
